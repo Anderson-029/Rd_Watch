64aefe9f4fb113f49753783e6a765ae153cedf3a
Anderson-029 <anderdev29@gmail.com>
2025-11-17 11:57:45 -0500

    update

--- 3.dependencias transitivas.sql
+++
@@ -1,9 +0,0 @@
--- Buscar posibles dependencias transitivas (columnas que podrían derivarse de otras)
--- Esta es una consulta conceptual que requiere adaptación según esquema
-SELECT t1.table_name, t1.column_name as posible_dependiente, 
-       t2.column_name as posible_determinante
-FROM information_schema.columns t1
-JOIN information_schema.columns t2 
-  ON t1.table_name = t2.table_name
-WHERE t1.column_name LIKE '%total%' 
-  AND (t2.column_name LIKE '%quantity%' OR t2.column_name LIKE '%price%');
--- 5 deteccion de datos duplicados en usuarios.sql
+++
@@ -1,5 +0,0 @@
--- Posibles datos duplicados (ejemplo para tabla de clientes)
-SELECT first_name, last_name, email, COUNT(*) as duplicates
-FROM customers
-GROUP BY first_name, last_name, email
-HAVING COUNT(*) > 1;
